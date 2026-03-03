bff7bdcf5fc0f5deca793f7916962a5165b7e338
Shrey-2828 <ldce.2324@gmail.com>
2026-03-03 16:19:14 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import pickle
+
+#load model
+model=pickle.load(open('pipe.pkl','rb'))
+
+def predict(data):
+    
+    ans = model.predict(data)
+    
+    if ans[0]==1:
+     return 'Survived ✅'
+    
+    else:
+     return 'Not Survived ❌'
+    
+    
+def main():
+     st.title('Titanic Survival Chance !!!')  
+     
+     Pclass = st.selectbox("Passenger Class", [1,2,3])
+     Gender = st.selectbox("Gender", ["male","female"])
+     Age = st.number_input("Age", min_value=0,max_value=110)
+     fare = st.number_input("Fare", min_value=0.0)
+     Embarked = st.selectbox("Embarked", ["S","Q","C"])     
+     
+     data= pd.DataFrame({
+        "Pclass": [Pclass],
+        "Sex": [Gender],
+        "Age": [Age],
+        "Fare": [fare],
+        "Embarked": [Embarked]
+    })
+     
+     if st.button('predict'):
+         result=predict(data)
+         st.success(result)
+         
+if __name__ == '__main__':
+  main()
+          
+         
