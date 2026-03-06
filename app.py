8d66f02f60490eb285cc4fa8aedb0175cf8a49bb
Shrey-2828 <ldce.2324@gmail.com>
2026-03-06 16:34:53 +0530

    Fix predict function and add return statement

--- app.py
+++ app.py
@@ -3,38 +3,39 @@ import numpy as np
 import pandas as pd
 import pickle
 
-#load model
-model=pickle.load(open('pipe.pkl','rb'))
 
-def predict(data): 
+model = pickle.load(open('pipe.pkl', 'rb'))
+
+def predict(data):
     ans = model.predict(data)
-        
+    return ans[0]  
+
+
 def main():
-     st.title('Titanic Survival Chance !!!')  
-     
-     Pclass = st.selectbox("Passenger Class", [1,2,3])
-     Gender = st.selectbox("Gender", ["male","female"])
-     Age = st.number_input("Age", min_value=0,max_value=110)
-     fare = st.number_input("Fare", min_value=0.0)
-     Embarked = st.selectbox("Embarked", ["S","Q","C"])     
-     
-     data= pd.DataFrame({
+    st.title('Titanic Survival Chance !!!')
+
+    Pclass = st.selectbox("Passenger Class", [1, 2, 3])
+    Gender = st.selectbox("Gender", ["male", "female"])
+    Age = st.number_input("Age", min_value=0, max_value=110)
+    fare = st.number_input("Fare", min_value=0.0)
+    Embarked = st.selectbox("Embarked", ["S", "Q", "C"])
+
+    data = pd.DataFrame({
         "Pclass": [Pclass],
         "Sex": [Gender],
         "Age": [Age],
         "Fare": [fare],
         "Embarked": [Embarked]
-     })
-     
-     if st.button('predict'):
-         result=predict(data)
-         
-         if result ==1:
-              st.success("Survived ✅") 
-         else:
-             st.error( 'Not Survived ❌')
-         
-         
+    })
+
+    if st.button('Predict'):
+        result = predict(data)
+
+        if result == 1:
+            st.success("Survived ✅")
+        else:
+            st.error("Not Survived ❌")
+
+
 if __name__ == '__main__':
-  main()
-          
+    main()
